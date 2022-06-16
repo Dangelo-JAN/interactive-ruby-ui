c517a352299889e0ced32a64ad1457980d1e43b5
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-06-16 17:09:28 -0400

    Library UI

---
+++ app.rb
@@ -0,0 +1,110 @@
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'rental'
+require_relative 'main'
+
+class App
+  attr_accessor :books, :people, :rentals
+  attr_reader :main
+
+  def initialize(books = [], people = [], rentals = [])
+    @books = books
+    @people = people
+    @rentals = rentals
+  end
+
+  def book_list
+    puts 'Books list'
+    @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    main.main
+  end
+
+  def people_list
+    puts 'People list'
+    @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    main.main
+  end
+
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_type = gets.chomp.to_i
+    case person_type
+    when '1' then create_student
+    when '2' then create_teacher
+    else
+      puts 'Invalid selection'
+      create_person
+    end
+    puts 'Person created successfully'
+    main.main
+  end
+
+  def create_student
+    puts 'Age: '
+    age = gets.chomp.to_i
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Do you have permission[Y/N]: '
+    parent_permission = gets.chomp
+    student = Student.new(age, name, parent_permission: parent_permission)
+    people << student unless @people.include?(student)
+  end
+
+  def create_teacher
+    puts 'Age: '
+    age = gets.chomp.to_i
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Do you have specialization[Y/N]: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    people << teacher unless @people.include?(teacher)
+  end
+
+  def create_book
+    puts 'Title: '
+    title = gets.chomp
+    puts 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    books << book unless @books.include?(book)
+    puts 'Book created successfully'
+    main.main
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    books.each_with_index do |book, index|
+      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+    end
+    book_selection = gets.chomp.to_i
+    puts
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_selection = gets.chomp.to_i
+    puts ''
+    print('Enter the date of the rental (YYYY-MM-DD): ')
+    rental_date = gets.chomp
+    rental = Rental.new(rental_date, books[book_selection], people[person_selection])
+    rentals << rental
+    puts 'Rental created successfully'
+    main.main
+  end
+
+  def rentals_from_person_id
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    print 'Enter the id of the person: '
+    person_selection = gets.chomp.to_i
+    puts 'Rentals:'
+    person = people[person_selection]
+    person.rentals.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+    end
+    main.main
+  end
+end
--- main.rb
+++ main.rb
@@ -1,97 +1,41 @@
-require_relative 'teacher'
-require_relative 'student'
-require_relative 'rental'
+require 'app'
 
-class ClassName
-  def initialize
-    
-  end
+def main # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
+  class << self
+    def initialize
+      app = App.new
+    end
+    def intro_app
+      puts 'Welcome to School Library App/n/n'
 
-  def intro_app
-  puts 'Welcome to School Library App/n/n'
+      puts 'Please choose an option enterin a number:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
+      enterin_selection
+    end
 
-  puts 'Please choose an option enterin a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-  end
-
-  def enterin_selection
-    option = gets.chomp.to_i
-
-    case option
-    when 1
-      book_list
-    when 2
-      people_list
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      rentals_from_person_id
-    when 7
-      exit(true)
-    when String
-      "You passed a string"
-    else
-      "You gave me #{x} -- I have no idea what to do with that."
-    end   
-  end
-
-  def book_list
-    puts "Books list" 
-  end
-
-  def people_list
-    puts "People list"    
-  end
-
-  def create_person
-    puts "Do you want to create a student (1) or a teacher (2)? [Input the number]: " person_type = gets.chomp.to_i
-    puts "Age: " age = gets.chomp.to_i
-    puts "Name: " name = gets.chomp.to_i
-    puts "Do oyu have permission[Y/N]: " permission = gets.chomp.to_s    
-  end
-
-  def create_book
-    
-  end
-
-  def create_rental
-    
-  end
-
-  def rentals_from_person_id
-    
+    def enterin_selection # rubocop:disable Metrics/CyclomaticComplexity,
+      option = gets.chomp.to_i
+      case option
+      when 1 then app.book_list
+      when 2 then app.people_list
+      when 3 then app.create_person
+      when 4 then app.create_book
+      when 5 then app.create_rental
+      when 6 then app.rentals_from_person_id
+      when 7 then exit(true)
+      else
+        puts 'Sorry, you choose a wrong option'
+        puts ''
+        enterin_selection
+      end
+    end
   end
 end
 
-
-
-
-def main
-  app = App.new()
-  app.run()
-end
-
-main()
-
-# class App
-#   ...
-# end
-
-# ...
-
-# def main
-#   app = App.new()
-#   app.run()
-# end
-
-# main()
+main
